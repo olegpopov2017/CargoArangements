1097cb8101bfce72b44380b25d23f4bd79299522
oleg <olegpopov2017@mail.ru>
2025-02-16 11:39:00 +0300

    test commit

---
+++ react_CA/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+let a = 56
+let arr = [1, 2, 3]
+
+function App() {
+  return (
+    <div>
+      {arr}
+      <h1>Vite + React + Cargo{a}</h1>
+      et{' '}
+    </div>
+  )
+}
+
+export default App
