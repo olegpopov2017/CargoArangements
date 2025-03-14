3247207e9dc4c660c65d22228fa49a79d8d54bc5
oleg <olegpopov2017@mail.ru>
2025-03-14 06:35:37 +0300

    create internet fetch algoritm

--- study vite/study_vite/NEW_functions.js
+++ study vite/study_vite/NEW_functions.js
@@ -1,117 +1,208 @@
-import * as THREE from 'three';
-import { create_cuboid_from_cargo_area,create_cargo_from_cuboid,threejs_scena_to_cuboid_with_inner_objects,
-        create_cuboid_from_cargo,cargo_area_adding_from_cuboid, } from './functions';
-import {Cuboid} from './classes.js';
-import {cargo_area_group,cargo_group,group_of_cargo_area_floor,group_of_cargo_area_attribute} from './three_cargo_canvas.js';
+import * as THREE from 'three'
+import {
+  create_cuboid_from_cargo_area,
+  create_cargo_from_cuboid,
+  threejs_scena_to_cuboid_with_inner_objects,
+  create_cuboid_from_cargo,
+  cargo_area_adding_from_cuboid,
+} from './functions'
+import { Cuboid } from './classes.js'
+import {
+  cargo_area_group,
+  cargo_group,
+  group_of_cargo_area_floor,
+  group_of_cargo_area_attribute,
+} from './three_cargo_canvas.js'
 // import{addcolumn} from './Table_of_cargo_parameters.js'
 
-      
-
 //Send data of scena(cargo area,cargos) to local algorithm and come back result on the screen.
-export function placement_cargo_according_to_algorithm_on_local_server()
-    {
-    let box = threejs_scena_to_cuboid_with_inner_objects()
-    console.log(box)
-    
-    fetch('http://127.0.0.1:3000',{method: 'post',body: JSON.stringify(box)})
-        .then((response) => {
-            
-            return response.json();
-        })
-            .then((data) => {
-                
-                cargo_group.clear()
-                cargo_area_group.clear()
-                group_of_cargo_area_floor.clear()
-                group_of_cargo_area_attribute.clear()
+export function placement_cargo_according_to_algorithm_on_internet_server() {
+  let box = threejs_scena_to_cuboid_with_inner_objects()
+  console.log(box)
 
-                
-                let cube = new Cuboid
+  fetch('http://185.253.182.115/api/algotitm', {
+    method: 'post',
+    body: JSON.stringify(box),
+  })
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      cargo_group.clear()
+      cargo_area_group.clear()
+      group_of_cargo_area_floor.clear()
+      group_of_cargo_area_attribute.clear()
 
-                cube.uuid = data.uuid
-                cube.width_X = data.width_X
-                cube.height_Y = data.height_Y
-                cube.depth_Z = data.depth_Z
-                cube.position_x = data.x
-                cube.position_y = data.y
-                cube.position_z = data.z
+      let cube = new Cuboid()
 
-                let parent_cube = cargo_area_adding_from_cuboid(cube)
-                                
-                cargo_area_group.add(parent_cube);
-                
-                for(let i = 0; i < data.array_of_inner_objects.length; i++)
-                {
-                    let inner_cube = data.array_of_inner_objects[i]
-                    
-                    let children_cube = new Cuboid
-                    
-                    children_cube.uuid = inner_cube.uuid
-                    children_cube.width_X = inner_cube.width_X
-                    children_cube.height_Y = inner_cube.height_Y
-                    children_cube.depth_Z = inner_cube.depth_Z
-                    children_cube.position_x = Number(inner_cube.position_x)
-                    children_cube.position_y = Number(inner_cube.position_y)
-                    children_cube.position_z = Number(inner_cube.position_z)
-                    
-                    let children = create_cargo_from_cuboid(children_cube)
-                    
-                    cargo_group.add(children);
-                    
-                }
-                
-                return data
-                               
-            }).then(present_object_parameters)
-    }
+      cube.uuid = data.uuid
+      cube.width_X = data.width_X
+      cube.height_Y = data.height_Y
+      cube.depth_Z = data.depth_Z
+      cube.position_x = data.x
+      cube.position_y = data.y
+      cube.position_z = data.z
 
+      let parent_cube = cargo_area_adding_from_cuboid(cube)
 
+      cargo_area_group.add(parent_cube)
 
+      for (let i = 0; i < data.array_of_inner_objects.length; i++) {
+        let inner_cube = data.array_of_inner_objects[i]
+
+        let children_cube = new Cuboid()
+
+        children_cube.uuid = inner_cube.uuid
+        children_cube.width_X = inner_cube.width_X
+        children_cube.height_Y = inner_cube.height_Y
+        children_cube.depth_Z = inner_cube.depth_Z
+        children_cube.position_x = Number(inner_cube.position_x)
+        children_cube.position_y = Number(inner_cube.position_y)
+        children_cube.position_z = Number(inner_cube.position_z)
+
+        let children = create_cargo_from_cuboid(children_cube)
+
+        cargo_group.add(children)
+      }
+
+      return data
+    })
+    .then(present_object_parameters)
+}
+//Send data of scena(cargo area,cargos) to local algorithm and come back result on the screen.
+export function placement_cargo_according_to_algorithm_on_local_server() {
+  let box = threejs_scena_to_cuboid_with_inner_objects()
+  console.log(box)
+
+  fetch('http://127.0.0.1:3000', { method: 'post', body: JSON.stringify(box) })
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      cargo_group.clear()
+      cargo_area_group.clear()
+      group_of_cargo_area_floor.clear()
+      group_of_cargo_area_attribute.clear()
+
+      let cube = new Cuboid()
+
+      cube.uuid = data.uuid
+      cube.width_X = data.width_X
+      cube.height_Y = data.height_Y
+      cube.depth_Z = data.depth_Z
+      cube.position_x = data.x
+      cube.position_y = data.y
+      cube.position_z = data.z
+
+      let parent_cube = cargo_area_adding_from_cuboid(cube)
+
+      cargo_area_group.add(parent_cube)
+
+      for (let i = 0; i < data.array_of_inner_objects.length; i++) {
+        let inner_cube = data.array_of_inner_objects[i]
+
+        let children_cube = new Cuboid()
+
+        children_cube.uuid = inner_cube.uuid
+        children_cube.width_X = inner_cube.width_X
+        children_cube.height_Y = inner_cube.height_Y
+        children_cube.depth_Z = inner_cube.depth_Z
+        children_cube.position_x = Number(inner_cube.position_x)
+        children_cube.position_y = Number(inner_cube.position_y)
+        children_cube.position_z = Number(inner_cube.position_z)
+
+        let children = create_cargo_from_cuboid(children_cube)
+
+        cargo_group.add(children)
+      }
+
+      return data
+    })
+    .then(present_object_parameters)
+}
 
 //Present parameters of cargo area and cargos in console.
-export function present_object_parameters()
-    {
-        
-        try{
-            console.clear()
-            
-            let car_area = create_cuboid_from_cargo_area(cargo_area_group.children[0])
-            
-            if(typeof(car_area) === "undefined"){throw new Error("Cargo area not createted. Please create cargo area and try again.")}
+export function present_object_parameters() {
+  try {
+    console.clear()
 
-            if( car_area.width_X == 0 ||
-            car_area.height_Y == 0 ||
-            car_area.depth_Z == 0)
-            {throw new Error("Cargo area size not valid. Please enter correct size and try again.")}
-            
-            if(cargo_group.children.length == 0) {throw new Error("No objects in cargo area.")}
+    let car_area = create_cuboid_from_cargo_area(cargo_area_group.children[0])
 
-            console.log(' Cargo area UUID: ',car_area.uuid,'\n',
-                            'Cargo area size:  ','Width =',car_area.width_X,'; Height =',car_area.height_Y,'; depth =',car_area.depth_Z,'\n',
-                            'Number of objects in the cargo area:',cargo_group.children.length,'\n',)
-                    
-
-
-                for (let i = 0; i < cargo_group.children.length; ++i)
-                    {
-                    let cargo = create_cuboid_from_cargo(cargo_group.children[i]);
-
-                    if( cargo.width_X == 0 ||
-                        cargo.height_Y == 0 ||
-                        cargo.depth_Z == 0)
-                    {console.log("Cargo uuid",cargo.uuid ,"size not valid. Please enter correct size and try again.")}
-                    
-                    console.log('     Number of object: ',i+1,'\n',
-                                '    UUID: ',cargo.uuid,'\n',
-                                '    Size:  ','Width =',cargo.width_X,'; Height =',cargo.height_Y,'; Depth =',cargo.depth_Z,'\n',
-                                '    Coordinates:  ','X =',cargo.position_x,'; Y =',cargo.position_y, '; Z =',cargo.position_z
-                                );
-                    
-                    // animate()
-                    }
-            } 
-            catch (err) 
-            {
-                // console.log(err.message)
-            }
+    if (typeof car_area === 'undefined') {
+      throw new Error(
+        'Cargo area not createted. Please create cargo area and try again.'
+      )
     }
+
+    if (
+      car_area.width_X == 0 ||
+      car_area.height_Y == 0 ||
+      car_area.depth_Z == 0
+    ) {
+      throw new Error(
+        'Cargo area size not valid. Please enter correct size and try again.'
+      )
+    }
+
+    if (cargo_group.children.length == 0) {
+      throw new Error('No objects in cargo area.')
+    }
+
+    console.log(
+      ' Cargo area UUID: ',
+      car_area.uuid,
+      '\n',
+      'Cargo area size:  ',
+      'Width =',
+      car_area.width_X,
+      '; Height =',
+      car_area.height_Y,
+      '; depth =',
+      car_area.depth_Z,
+      '\n',
+      'Number of objects in the cargo area:',
+      cargo_group.children.length,
+      '\n'
+    )
+
+    for (let i = 0; i < cargo_group.children.length; ++i) {
+      let cargo = create_cuboid_from_cargo(cargo_group.children[i])
+
+      if (cargo.width_X == 0 || cargo.height_Y == 0 || cargo.depth_Z == 0) {
+        console.log(
+          'Cargo uuid',
+          cargo.uuid,
+          'size not valid. Please enter correct size and try again.'
+        )
+      }
+
+      console.log(
+        '     Number of object: ',
+        i + 1,
+        '\n',
+        '    UUID: ',
+        cargo.uuid,
+        '\n',
+        '    Size:  ',
+        'Width =',
+        cargo.width_X,
+        '; Height =',
+        cargo.height_Y,
+        '; Depth =',
+        cargo.depth_Z,
+        '\n',
+        '    Coordinates:  ',
+        'X =',
+        cargo.position_x,
+        '; Y =',
+        cargo.position_y,
+        '; Z =',
+        cargo.position_z
+      )
+
+      // animate()
+    }
+  } catch (err) {
+    // console.log(err.message)
+  }
+}
--- study vite/study_vite/tags_buttons_DOM.js
+++ study vite/study_vite/tags_buttons_DOM.js
@@ -1,23 +1,31 @@
-import * as THREE from 'three';
-import {cargo_area_group,cargo_group,group_of_cargo_area_floor,group_of_cargo_area_attribute} from './three_cargo_canvas.js';
-import {cargo_area_adding,resize_renderer,create_cargo_after_input_data_and_adding_to_scene} from './functions.js';
-import {placement_cargo_according_to_algorithm_on_local_server,present_object_parameters} from './NEW_functions.js';
-import {import_from_excel,export_to_excel} from './excel.js';
-import {testfunc} from './Table_of_cargo_parameters.js'
+import * as THREE from 'three'
+import {
+  cargo_area_group,
+  cargo_group,
+  group_of_cargo_area_floor,
+  group_of_cargo_area_attribute,
+} from './three_cargo_canvas.js'
+import {
+  cargo_area_adding,
+  resize_renderer,
+  create_cargo_after_input_data_and_adding_to_scene,
+} from './functions.js'
+import {
+  placement_cargo_according_to_algorithm_on_local_server,
+  present_object_parameters,
+  placement_cargo_according_to_algorithm_on_internet_server,
+} from './NEW_functions.js'
+import { import_from_excel, export_to_excel } from './excel.js'
+import { testfunc } from './Table_of_cargo_parameters.js'
 
+//Adding button "f" to change screen size.
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'f') {
+    resize_renderer()
+  }
+})
 
-
-
-		//Adding button "f" to change screen size.
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'f') 
-	{
-        resize_renderer()
-    }
-});
-
-		
-		//Creating Form2 - Adding/delete cargo area to scene
+//Creating Form2 - Adding/delete cargo area to scene
 let form2_html = `
 <fieldset>
 <legend>Создание грузового помещения</legend>
@@ -42,26 +50,30 @@ let form2_html = `
         </tr>
     </table>
 </fieldset>
-    `;
-    
-let form2 = document.createElement('form2');
-form2.id = "form2";
-form2.innerHTML = form2_html;
-document.body.append(form2);
+    `
+
+let form2 = document.createElement('form2')
+form2.id = 'form2'
+form2.innerHTML = form2_html
+document.body.append(form2)
 
 //Button 24 - Adding/change cargo area
-let button24 = document.querySelector('#button24');
-button24.addEventListener('click',cargo_area_adding);
+let button24 = document.querySelector('#button24')
+button24.addEventListener('click', cargo_area_adding)
 
 //Button 25 - Delete all cargo areas in scene
-let button25 = document.querySelector('#button25');
-button25.addEventListener('click',() => (cargo_area_group.clear(),group_of_cargo_area_floor.clear(),group_of_cargo_area_attribute.clear(),present_object_parameters()));
+let button25 = document.querySelector('#button25')
+button25.addEventListener(
+  'click',
+  () => (
+    cargo_area_group.clear(),
+    group_of_cargo_area_floor.clear(),
+    group_of_cargo_area_attribute.clear(),
+    present_object_parameters()
+  )
+)
 
-
-
-
-
-			//Creating Form4 - Creating object,delete object on scene.
+//Creating Form4 - Creating object,delete object on scene.
 let form4_html = `
 <fieldset>
 				<legend>Создание грузов</legend>
@@ -120,43 +132,48 @@ let form4_html = `
 					<table/>
 					
     </fieldset>
-    `;
-        
-let form4 = document.createElement('form4');
-form4.id = "form4";
-form4.innerHTML = form4_html;
-document.body.append(form4);
+    `
+
+let form4 = document.createElement('form4')
+form4.id = 'form4'
+form4.innerHTML = form4_html
+document.body.append(form4)
 
 //Button42(Form 4) - Creating object
-let button42 = document.querySelector('#button42');
-button42.addEventListener('click',testfunc);
+let button42 = document.querySelector('#button42')
+button42.addEventListener('click', testfunc)
 
 // button45.addEventListener('click',present_object_parameters)
-let button45 = document.querySelector('#button45');
-button45.addEventListener('click',() => (create_cargo_after_input_data_and_adding_to_scene(),present_object_parameters()))
+let button45 = document.querySelector('#button45')
+button45.addEventListener(
+  'click',
+  () => (
+    create_cargo_after_input_data_and_adding_to_scene(),
+    present_object_parameters()
+  )
+)
 
 //Button 43(Form4) - Delete all objects in scene
-let button43 = document.querySelector('#button43');
-button43.addEventListener('click',() => (cargo_group.clear(),present_object_parameters()));
+let button43 = document.querySelector('#button43')
+button43.addEventListener(
+  'click',
+  () => (cargo_group.clear(), present_object_parameters())
+)
 
 //Button 44(Form4) - Present object parameters
-let button44 = document.querySelector('#button44');
-button44.addEventListener('click',present_object_parameters);
+let button44 = document.querySelector('#button44')
+button44.addEventListener('click', present_object_parameters)
 
 //Button 44(Form4) - Present object parameters
-let button_LoadOnScreenMyFile = document.querySelector('#LoadOnScreenMyFile');
-button_LoadOnScreenMyFile.addEventListener('click',import_from_excel);
+let button_LoadOnScreenMyFile = document.querySelector('#LoadOnScreenMyFile')
+button_LoadOnScreenMyFile.addEventListener('click', import_from_excel)
 
-
-
-
-
-							//Creating Form5 - Placement cargo according to algorithm
+//Creating Form5 - Placement cargo according to algorithm
 let form5_html = `
 <fieldset>
 <legend>Автоматическая расстановка. Экспорт в Excel.</legend>
     <table>
-        <td><button id = "button53" type="button" class = "button1">Не работает</button></td>
+        <td><button id = "button53" type="button" class = "button1">Расчитать</button></td>
 		<td><button id = "button54" type="button" class = "button1">Расчитать на локальном сервере</button></td>
 	</table>
 	<br>
@@ -165,27 +182,27 @@ let form5_html = `
 		<td><button id = "button55" type="button" class = "button1">Экспорт параметров грузов в документ Excel</button></td>
 	</table>
 </fieldset>
-    `;
-    
-	
-let form5 = document.createElement('form5');
-form5.id = "form5";
-form5.innerHTML = form5_html;
-document.body.append(form5);
-               
+    `
+
+let form5 = document.createElement('form5')
+form5.id = 'form5'
+form5.innerHTML = form5_html
+document.body.append(form5)
+
 //Button 53 - creating buttons 'placement cargo according with algorithm"
-let button53 = document.querySelector('#button53');
-
+let button53 = document.querySelector('#button53')
+button53.addEventListener(
+  'click',
+  placement_cargo_according_to_algorithm_on_internet_server
+)
 
 //Button 54 - creating buttons 'placement cargo according with algorithm"
-let button54 = document.querySelector('#button54');
-button54.addEventListener('click',placement_cargo_according_to_algorithm_on_local_server);
+let button54 = document.querySelector('#button54')
+button54.addEventListener(
+  'click',
+  placement_cargo_according_to_algorithm_on_local_server
+)
 
 //Button 54 - creating buttons 'placement cargo according with algorithm"
-let button55 = document.querySelector('#button55');
-button55.addEventListener('click',export_to_excel);
-
-
-
-
-    
+let button55 = document.querySelector('#button55')
+button55.addEventListener('click', export_to_excel)
